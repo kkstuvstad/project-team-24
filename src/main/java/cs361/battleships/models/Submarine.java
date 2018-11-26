a82cab248d4c19303545b0a374e5dd06fb11cff5
nosnatef <gushua1984@gmail.com>
2018-11-26 11:50:49 -0800

    add extra square to submarine

--- src/main/java/cs361/battleships/models/Submarine.java
+++ src/main/java/cs361/battleships/models/Submarine.java
@@ -16,4 +16,22 @@ public class Submarine extends Ship {
             captainQuarter = new Square(row, (char) (col + 3));
         }
     }
+
+    @Override
+    public void place(char col, int row, boolean isVertical) {
+        for (int i=0; i<size; i++) {
+            if (isVertical) {
+                occupiedSquares.add(new Square(row+i, col));
+            } else {
+                occupiedSquares.add(new Square(row, (char) (col + i)));
+            }
+        }
+        placeCaptainQuarter(col, row, isVertical);
+        if(isVertical){
+            occupiedSquares.add(new Square(row + 2,(char)(col + 1)));
+        }
+        else{
+            occupiedSquares.add(new Square(row + 1,(char)(col + 2)));
+        }
+    }
 }
